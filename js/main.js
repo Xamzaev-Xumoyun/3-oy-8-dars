a4533adcb52d876ec5090f8c28626329b9dba3e9
Xamzaev-Xumoyun <xamzaevxumoyun49@gmail.com>
2022-03-01 00:28:14 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var elSpanMan = document.querySelector(".span__man");
 var elSpanBicycle = document.querySelector(".span__velosiped");
 var elSpanCar = document.querySelector(".span__car");
 var elSpanPilot = document.querySelector(".span__pilot");
+// var elInputVal = elInput.value;
+
+
 
 
 
@@ -12,38 +15,72 @@ var elSpanPilot = document.querySelector(".span__pilot");
 
 
 elForm.addEventListener( "submit", function(event){
-event.preventDefault();
+    event.preventDefault();
+    var elError = document.querySelector(".error");
+    var elInputVal = elInput.value;
+  
+    
+    if(elInputVal <= 0 || isNaN(elInputVal)){
 
-var elInputVal = elInput.value;
+        elError.textContent =   " 0  dan katta raqam kiriting , yoki 0 dan katta son  kiriting" ;
 
+        return;
+    
+      }else
+      elError.textContent = ""
+    
+    
+    
+    function distationman(){
+        var   house =Math.floor( elInputVal / 3.6);
+        var soat = ( elInputVal / 3.6).toFixed();
+        var   hourse =( elInputVal / 3.6).toFixed(2);
+        var  minut =( ((hourse - house)*60).toFixed() );
+        return soat + " soat " + minut + " minut ";
+    }
 
+    function distationbicycle(){
+        var   house =Math.floor( elInputVal / 20.1);
+        var soat = Math.round( elInputVal / 20.1);
+        var   hourse =( elInputVal / 20.1).toFixed(2);
+        var  minut =( ((hourse - house)*60).toFixed() );
+        return soat + " soat " + minut + " minut ";
+    }
 
-function man(){
-    return elInputVal / 3.6;
-}
-function bicycle(){
- return elInputVal / 20.1;
-}
+    function distationcar(){
+        var   house =Math.floor( elInputVal / 70);
+        var soat = Math.round( elInputVal / 70);
+        var   hourse =( elInputVal / 70).toFixed(2);
+        var  minut =( ((hourse - house)*60).toFixed() );
+        return soat + " soat " + minut + " minut ";
+    }
 
-function car(){
-    return elInputVal / 70;
-}
+    function distationpilot(){
+        var   house =Math.floor( elInputVal / 800);
+        var soat = Math.round( elInputVal / 800);
+        var   hourse =( elInputVal / 800).toFixed(2);
+        var  minut =( ((hourse - house)*60).toFixed() );
+        return soat + " soat " + minut + " minut ";
+        
+    }
 
-function pilot(){
-    return elInputVal/800;
-}
+    
 
-elSpanMan.textContent =(man().toFixed(2));
-elSpanBicycle.textContent = (bicycle().toFixed(2));
-elSpanCar.textContent = (car().toFixed(2));
-elSpanPilot.textContent =(pilot().toFixed(2));
-
+    elSpanMan.textContent =(distationman());
+    elSpanBicycle.textContent = (distationbicycle());
+    elSpanCar.textContent = (distationcar());
+    elSpanPilot.textContent =(distationpilot());
+    
 });
 
 
+// Math.round()
+// Math.floor()
+// Math.ceil()
 
 
 
+// Math.random()
 
 
 
